bf723771d7afb38b0d38f2b68795295b816d44a1
Fernando Calisto <fernandocalisto27@gmail.com>
2026-03-10 17:06:03 -0300

    feat: setup spring security and ninja controller class

---
+++ src/main/java/br/com/calistofernando/CadastroDeNinjas/Controller.java
@@ -0,0 +1,18 @@
+package br.com.calistofernando.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "This is my first route";
+    }
+
+
+}
