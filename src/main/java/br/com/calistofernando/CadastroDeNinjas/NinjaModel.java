8c72c29700b18fc15a2cedd5d50c92327bdeac73
Fernando Calisto <fernandocalisto27@gmail.com>
2026-03-10 17:43:49 -0300

    feat: create Ninja entity with name, age and email attributes

---
+++ src/main/java/br/com/calistofernando/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,49 @@
+package br.com.calistofernando.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_ninja_registration")
+
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private int age;
+
+    public NinjaModel(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public NinjaModel() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
