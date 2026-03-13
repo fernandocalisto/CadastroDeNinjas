fe716c6be74c25d83a01cc99be169c208991ba79
Fernando Calisto <fernandocalisto27@gmail.com>
2026-03-13 11:20:16 -0300

    feat: Add Lombok to the class for abstraction

--- src/main/java/br/com/calistofernando/CadastroDeNinjas/Missions/MissionModel.java
+++ src/main/java/br/com/calistofernando/CadastroDeNinjas/Missions/MissionModel.java
@@ -2,12 +2,17 @@ package br.com.calistofernando.CadastroDeNinjas.Missions;
 
 import br.com.calistofernando.CadastroDeNinjas.NInjas.NinjaModel;
 import jakarta.persistence.*;
-import org.yaml.snakeyaml.events.Event;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class MissionModel {
 
@@ -16,36 +21,7 @@ public class MissionModel {
     private Long id;
     private String name;
     private MissionDifficulty difficulty;
-
     // Uma missão pode ter vários ninjas
     @OneToMany(mappedBy = "mission")
     private List<NinjaModel> ninjas;
-
-    public MissionModel(String name, MissionDifficulty difficulty) {
-        this.name = name;
-        this.difficulty = difficulty;
-    }
-
-    public MissionModel() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public MissionDifficulty getDifficulty() {
-        return difficulty;
-    }
-
-    public void setDifficulty(MissionDifficulty difficulty) {
-        this.difficulty = difficulty;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
--- src/main/java/br/com/calistofernando/CadastroDeNinjas/NInjas/NinjaModel.java
+++ src/main/java/br/com/calistofernando/CadastroDeNinjas/NInjas/NinjaModel.java
@@ -2,11 +2,15 @@ package br.com.calistofernando.CadastroDeNinjas.NInjas;
 
 import br.com.calistofernando.CadastroDeNinjas.Missions.MissionModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_ninja_registration")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 
 public class NinjaModel {
 
@@ -16,43 +20,8 @@ public class NinjaModel {
     private String name;
     private String email;
     private int age;
-
-
     //Um ninja tem apenas uma única missão
     @ManyToOne
     @JoinColumn(name = "missions_id")
     private MissionModel mission;
-
-    public NinjaModel(String name, String email, int age) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
-
-    public NinjaModel() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
