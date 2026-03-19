c306eac8c01e383045ef69cc0d513171edcec9fd
Fernando Calisto <fernandocalisto27@gmail.com>
2026-03-19 13:27:37 -0300

    refactor(mission): integrar DTO e mapper no service e atualizar controller

--- src/main/java/br/com/calistofernando/CadastroDeNinjas/Missions/MissionController.java
+++ src/main/java/br/com/calistofernando/CadastroDeNinjas/Missions/MissionController.java
@@ -14,22 +14,22 @@ public class MissionController {
     private final MissionService missionService;
 
     @PostMapping("/create")
-    public MissionModel addMission(@RequestBody MissionModel missionModel) {
+    public MissionDTO addMission(@RequestBody MissionDTO missionModel) {
         return this.missionService.addMission(missionModel);
     }
 
     @GetMapping("/all")
-    public List<MissionModel> showAllMissions() {
+    public List<MissionDTO> showAllMissions() {
         return missionService.showAllMissions();
     }
 
     @GetMapping("/id/{id}")
-    public MissionModel getMissionByID(@PathVariable Long id) {
+    public MissionDTO getMissionByID(@PathVariable Long id) {
         return this.missionService.getMissionByID(id);
     }
 
     @PutMapping("/modify/{id}")
-    public MissionModel modifyMissionByID (@PathVariable Long id, @RequestBody MissionModel missionModel) {
+    public MissionDTO modifyMissionByID (@PathVariable Long id, @RequestBody MissionModel missionModel) {
         return this.missionService.modifyMissionByID(id, missionModel);
     }
 
