fcfdbfff463084b52a939323bf7bd3de0795f0de
Fernando Calisto <fernandocalisto27@gmail.com>
2026-03-20 10:43:15 -0300

    refactor(mission): add ResponseEntity to controller for better HTTP responses

--- src/main/java/br/com/calistofernando/CadastroDeNinjas/Missions/MissionController.java
+++ src/main/java/br/com/calistofernando/CadastroDeNinjas/Missions/MissionController.java
@@ -1,6 +1,8 @@
 package br.com.calistofernando.CadastroDeNinjas.Missions;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,27 +16,46 @@ public class MissionController {
     private final MissionService missionService;
 
     @PostMapping("/create")
-    public MissionDTO addMission(@RequestBody MissionDTO missionModel) {
-        return this.missionService.addMission(missionModel);
+    public ResponseEntity<String> addMission(@RequestBody MissionDTO missionModel) {
+        MissionDTO newMission = missionService.addMission(missionModel);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("The mission (" + newMission.getName() + ") was successfully created, with ID (" + newMission.getId() + ").");
     }
 
     @GetMapping("/all")
-    public List<MissionDTO> showAllMissions() {
-        return missionService.showAllMissions();
+    public ResponseEntity<List<MissionDTO>> showAllMissions() {
+        List<MissionDTO> missionDTOList = missionService.showAllMissions();
+        return ResponseEntity.ok(missionDTOList);
     }
 
     @GetMapping("/id/{id}")
-    public MissionDTO getMissionByID(@PathVariable Long id) {
-        return this.missionService.getMissionByID(id);
+    public ResponseEntity<?> getMissionByID(@PathVariable Long id) {
+        MissionDTO missionDTO = missionService.getMissionByID(id);
+        if (missionDTO != null){
+            return ResponseEntity.ok(missionDTO);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Mission was NOT FOUND!");
+        }
     }
 
     @PutMapping("/modify/{id}")
-    public MissionDTO modifyMissionByID (@PathVariable Long id, @RequestBody MissionModel missionModel) {
-        return this.missionService.modifyMissionByID(id, missionModel);
+    public ResponseEntity<?> modifyMissionByID (@PathVariable Long id, @RequestBody MissionModel missionModel) {
+        MissionDTO missionDTO = missionService.getMissionByID(id);
+        if (missionDTO != null){
+            MissionDTO savedMission = missionService.modifyMissionByID(id, missionModel);
+            return ResponseEntity.ok(savedMission);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Mission NOT FOUND!");
+        }
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteMission(@PathVariable Long id) {
-        this.missionService.deleteMissionByID(id);
+    public ResponseEntity<String> deleteMission(@PathVariable Long id) {
+        if (missionService.getMissionByID(id) != null){
+            missionService.deleteMissionByID(id);
+            return ResponseEntity.ok("Mission with ID: " + id + " was successfully deleted!");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("Mission with ID: " + id + " was NOT FOUND!");
     }
 }
